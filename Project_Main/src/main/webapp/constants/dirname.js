2d5b74474bde69061cc1676c6e631bfe50ab99c2
cl0ud <148cl0ud@gmail.com>
2025-03-05 17:45:28 +0900

    remove

--- Project_Main/src/main/webapp/constants/dirname.js
+++
@@ -1,6 +0,0 @@
-const url = new URL(window.location.href)
-const baseUrl = url.origin + "/Job_Project/";
-const pageUrl = baseUrl + "page/";
-const constantsUrl = baseUrl + "constants/";
-
-export {url, baseUrl, pageUrl, constantsUrl}
